d8d69ffee78f1ed586f8e32a8f758d708f318077
Emmanuel Orrego <Emmanuel.Orrego@teaminternational.com>
2020-10-01 18:33:27 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import pdfkit
+import re
+import os
+from os import listdir
+from os.path import isfile, join
+
+mypath = input("Enter the path of the files to convert\n") 
+output_path = input("Enter the output path\n") 
+
+files = os.listdir(mypath)
+print(files)
+
+options = {
+  "enable-local-file-access": None,
+  "load-media-error-handling": 'ignore',
+  'load-error-handling': 'ignore'
+}
+
+for item in files:
+    path = mypath +"\\"+ item 
+    name = os.listdir(path)
+    for html in name:
+        if '.html' in html:
+            file_path = path + "\\" + html
+            output = output_path + "\\" +item + ".pdf"
+            pdfkit.from_file(file_path,output,options=options,)
+
+
+
